a488468ba0eb0b867c6b4421b0f8a3aedc4f28c0
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-12-24 23:35:08 +0100

    Agregar archivos de proyecto.

---
+++ ENT/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ENT
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ LasMisionesDeTonySoprano/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LasMisionesDeTonySoprano.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
